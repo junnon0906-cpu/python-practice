99a70a6f636a474a67e627e8a9db1983556dd944
junnon0906-cpu <junnon0906@gmail.com>
2026-02-15 19:19:15 +0800

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -1,17 +1,1 @@
-import requests
 
-TOKEN = "AAHCleOidktsYOfZ7xxkbiqN8BIaCt2ew7w"
-CHAT_ID = "8463335795"
-
-message = "⚠️ 測試成功！這是我的課表提醒 Bot"
-
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-data = {
-    "chat_id": CHAT_ID,
-    "text": message
-}
-
-response = requests.post(url, data=data)
-
-print("已送出通知")
