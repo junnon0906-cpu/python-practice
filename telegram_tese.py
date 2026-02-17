5b973c02c240f897b36c0066eab4fdd837c1f513
junnon0906-cpu <junnon0906@gmail.com>
2026-02-18 02:15:25 +0800

    Update telegram_tese.py

--- telegram_tese.py
+++ telegram_tese.py
@@ -1,21 +1,14 @@
 import requests
 
-TOKEN = "AAHCleOidktsYOfZ7xxkbiqN8BIaCt2ew7w"
+TOKEN = "8489882646:AAHCleOidktsYOfZ7xxkbiqN8BIaCt2ew7w"
 CHAT_ID = "8463335795"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 payload = {
     "chat_id": CHAT_ID,
-    "text": "✅ 校園網路測試成功！"
+    "text": "📚 課表提醒測試：如果你看到這行，代表一切正常"
 }
 
-response = requests.post(
-    url,
-    json=payload,
-    timeout=10,
-    verify=False   # 🔴 關鍵：關掉 SSL 驗證
-)
-
-print("HTTP:", response.status_code)
-print(response.text)
+r = requests.post(url, json=payload, timeout=10)
+print(r.json())
